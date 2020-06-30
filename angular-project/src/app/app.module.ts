296b004cd6cd44d34c349b59aa41dbdbc83a1e3a
lucasdemoraesc <lucasdemoraesc@gmail.com>
2020-06-30 11:37:03 -0300

    Incrementando roteamento

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
